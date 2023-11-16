1d0c2cc3304ed63ca9586545279c51ef63675b7f
Npal21 <nbpal21@gmail.com>
2023-11-16 12:36:49 +0530

    Added logic to customRender function

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,5 +1,16 @@
-function customRender(reactElement, container){
 
+//This function injects reactElement into the container
+function customRender(reactElement, container){
+    const domElement = document.createElement(reactElement.type);       //khali container
+    domElement.innerHTML = reactElement.children;
+    //domElement.setAttribute('href',reactElement.props.href)
+    //domElement.setAttribute('target',reactElement.props.target)
+
+    for (const prop in reactElement.props) {
+        if(prop === 'children') continue;
+        domElement.setAttribute(prop, reactElement.props[prop]);
+    }
+    container.appendChild(domElement)
 }
 
 const reactElement = {
@@ -14,3 +25,6 @@ const reactElement = {
 const mainContainer = document.getElementById("root")
 
 customRender(reactElement, mainContainer);
+
+//Note: 
+//{} is called as "evaluated expression" => It calculates the value of variable inside braces
